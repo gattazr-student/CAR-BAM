ee28ce534c931287b3e3ed3182d3403c1da7c3a2
Elsa Navarro <elsa0navarro@gmail.com>
2016-02-29 09:06:34 +0100

    Creation of classes Agent and AgentServer

---
+++ CAR-BAM/src/jus/aor/mobilagent/kernel/Agent.java
@@ -0,0 +1,44 @@
+package jus.aor.mobilagent.kernel;
+
+public class Agent implements _Agent, _Service<_Agent> {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -1463619481727183159L;
+
+	public Agent() {
+		// TODO create constructor
+	}
+
+	@Override
+	public void addEtape(Etape etape) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public _Agent call(Object... params) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(AgentServer agentServer, String serverName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reInit(AgentServer server, String serverName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CAR-BAM/src/jus/aor/mobilagent/kernel/AgentServer.java
@@ -0,0 +1,15 @@
+package jus.aor.mobilagent.kernel;
+
+public class AgentServer implements _Service<AgentServer> {
+
+	protected AgentServer(int i, String s) {
+		// TODO Make constructor
+	}
+
+	@Override
+	public AgentServer call(Object... params) throws IllegalArgumentException {
+		// TODO Make function
+		return null;
+	}
+
+}
