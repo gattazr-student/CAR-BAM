ec90338eaece371d74c1ecf23598954cbf015173
Quentin Dunand <quentindunand@gmail.com>
2016-03-31 17:57:38 +0200

    Annuaire.java done (have to test it)

--- CAR-BAM/src/jus/aor/rmi/server/Annuaire.java
+++ CAR-BAM/src/jus/aor/rmi/server/Annuaire.java
@@ -1,24 +1,61 @@
 package jus.aor.rmi.server;
 
+import java.io.File;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import jus.aor.rmi.common.Numero;
 import jus.aor.rmi.common._Annuaire;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class Annuaire extends UnicastRemoteObject implements _Annuaire {
 
 	private static final long serialVersionUID = -8828635014943186320L;
+	private HashMap<String, Numero> annuaire = new HashMap<String, Numero>();
 
-	protected Annuaire() throws RemoteException {
-		super();
-		// TODO Auto-generated constructor stub
+	protected Annuaire(String pathFile) throws RemoteException, IOException,
+			SAXException {
+		DocumentBuilder documentBuilder = null;
+		Document document = null;
+		String name = "";
+		String num = "";
+		NodeList telList = null;
+		NamedNodeMap attributes = null;
+
+		// Récup info depuis xml
+		try {
+			documentBuilder = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder();
+			document = documentBuilder.parse(new File(pathFile));
+
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+
+		telList = document.getElementsByTagName("telephone");
+
+		for (int i = 0; i < telList.getLength(); i++) {
+			attributes = telList.item(i).getAttributes();
+			name = attributes.getNamedItem("name").getNodeName();
+			num = attributes.getNamedItem("numero").getNodeValue();
+			this.annuaire.put(name, new Numero(num));
+		}
+
 	}
 
 	@Override
 	public Numero get(String abonne) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.annuaire.get(abonne);
 	}
 
 }
